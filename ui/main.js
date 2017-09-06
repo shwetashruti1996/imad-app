4b1189b93ab7bfbd9cb406f2f30461710cd48332
shwetashruti1996 <31120679+shwetashruti1996@users.noreply.github.com>
2017-09-06 15:03:56 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,14 @@
 console.log('Loaded!');
+
+//change the text of main-text
+
+var element= document.getElementById('main-text');
+
+element.innerHTML='New Value';
+
+//move the image
+var img=document.getElementById('img');
+img.onClick=function() {
+    img.style.marginLeft='100px'
+    
+}
