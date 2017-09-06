a0123259d7ddbd1de2220f31f324de6b2d5b309f
shwetashruti1996 <31120679+shwetashruti1996@users.noreply.github.com>
2017-09-06 15:24:57 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -7,9 +7,14 @@ element.innerHTML='New Value';
 
 //move the image
 var img=document.getElementById('madi');
-
+var marginLeft=0;
+function() {
+     marginLeft= marginLeft + 10;
+    img.style.marginLeft=marginLeft + 'px';
+    
+    
+}
 img.onclick=function() {
-   // var interval=setInterval
-    img.style.marginLeft='100px';
+    var interval=setInterval(moveRight, 100);
     
 };
